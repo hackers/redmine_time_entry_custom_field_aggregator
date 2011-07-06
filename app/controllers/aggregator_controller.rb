8fa4cbc727fa0589b48a9d43186f65c9bf0b731a
yono.05@gmail.com <root@redmine.es.occ.co.jp>
2011-07-06 23:50:43 +0900

    一ヶ月分の表を出力

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -1,11 +1,60 @@
 class AggregatorController < ApplicationController
   unloadable
 
-
   def index
-      @projects = Project.all
-      if params.has_key? :project
-        @project = Project.find(params[:project][:id])
+    @projects = Project.all
+    if params.has_key? :project
+      @project = Project.find(params[:project][:id])
+    end
+
+    if params.has_key? :user
+      @user = User.find(params[:user][:id])
+    end
+
+    if @project and @user
+      @custom_fields = CustomField.find(:all, 
+                                        :conditions => {:type => 'TimeEntryCustomField', 
+                                                        :field_format => 'float'},
+                                        :order => :position)
+      @headers = ['時間']
+      @custom_fields.each do |custom_field|
+        @headers << custom_field.name
       end
+ 
+      ## 日ごとのデータ集計
+      @sum_all = Array.new(@headers.length, 0)
+
+      @month_index = (Date.today.beginning_of_month .. Date.today.at_end_of_month).to_a
+      @month_index = @month_index.map {|x| x.to_s}
+
+      @data_table = Hash[*@month_index.zip(Array.new(@month_index.length, 0)).flatten]
+      @data_table.each do |key, val|
+        @data_table[key] = Array.new(@headers.length, 0)
+      end
+
+      @entries = TimeEntry.find(:all, 
+                                :conditions => {:user_id => @user, 
+                                                :project_id => @project })
+      @entries.each do |entry|
+        if not @data_table.has_key? entry.spent_on.to_s
+          continue
+        end
+        @data_table[entry.spent_on.to_s][0] += entry.hours
+        @sum_all[0] += entry.hours
+        i = 1
+        @custom_fields.each do |custom_field|
+          custom_values = CustomValue.find(:all, 
+                                           :conditions => 
+                                             {:customized_id => entry.id, 
+                                              :custom_field_id => custom_field.id})
+          custom_values.each do |custom_value|
+            @data_table[entry.spent_on.to_s][i] += custom_value.value.to_f
+            @sum_all[i] += custom_value.value.to_f
+          end
+          i += 1
+        end
+      end
+     
+    end # if project and user
   end
 end
