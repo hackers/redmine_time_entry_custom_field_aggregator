f1aff6009cf81ad644cbd8224d74a1cda2c3bc6a
yono.05@gmail.com <yono.05@gmail.com>
2011-07-10 02:15:30 +0900

    add this plugin to module

--- init.rb
+++ init.rb
@@ -7,6 +7,8 @@ Redmine::Plugin.register :redmine_custom_field_aggregator do
   version '0.0.1'
   url 'http://github.com/hackers/redmine_custom_field_aggregator'
   author_url 'http://github.com/hackers'
-  permission :view_table, {:aggregator => [:index]}, :public => true
-  menu :project_menu, :view_table, { :controller => 'aggregator', :action => 'index'}, :caption => '時間集計', :param => :project_id
+  project_module :custom_field_aggregator do
+    permission :view_table, {:aggregator => [:index]}, :public => true
+    menu :project_menu, :view_table, { :controller => 'aggregator', :action => 'index'}, :caption => '時間集計', :param => :project_id
+  end
 end
