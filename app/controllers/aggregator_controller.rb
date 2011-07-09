ad549a1fb6d3eb3fae8e339971781fc67bec8e6b
yono.05@gmail.com <yono.05@gmail.com>
2011-07-09 19:19:27 +0900

    日付（年、月）の選択を実装

--- app/controllers/aggregator_controller.rb
+++ app/controllers/aggregator_controller.rb
@@ -1,17 +1,24 @@
 class AggregatorController < ApplicationController
   unloadable
 
+  YEAR_MIN = 2000
+  YEAR_RANGE = 10
+
   def index
     @projects = Project.all
+
     if params.has_key? :project
       @project = Project.find(params[:project][:id])
+      @years = (Date::new(YEAR_MIN).year .. Date.today.year + YEAR_RANGE).to_a
+      @months = (1 .. 12).to_a
     end
 
-    if params.has_key? :user
-      @user = User.find(params[:user][:id])
-    end
+    @user = User.find(params[:user][:id]) if params.has_key? :user
+    @year = params.has_key?(:year) ? params[:year][:id].to_i : Date.today.year
+    @month = params.has_key?(:month) ? params[:month][:id].to_i : Date.today.month
 
     if @project and @user
+
       @custom_fields = CustomField.find(:all, 
                                         :conditions => {:type => 'TimeEntryCustomField', 
                                                         :field_format => 'float'},
@@ -24,7 +31,8 @@ class AggregatorController < ApplicationController
       ## 日ごとのデータ集計
       @sum_all = Array.new(@headers.length, 0)
 
-      @month_index = (Date.today.beginning_of_month .. Date.today.at_end_of_month).to_a
+      selected_date = Date::new(@year.to_i, @month.to_i)
+      @month_index = (selected_date.beginning_of_month .. selected_date.at_end_of_month).to_a
       @month_index = @month_index.map {|x| x.to_s}
 
       @data_table = Hash[*@month_index.zip(Array.new(@month_index.length, 0)).flatten]
@@ -37,7 +45,7 @@ class AggregatorController < ApplicationController
                                                 :project_id => @project })
       @entries.each do |entry|
         if not @data_table.has_key? entry.spent_on.to_s
-          continue
+          next
         end
         @data_table[entry.spent_on.to_s][0] += entry.hours
         @sum_all[0] += entry.hours
