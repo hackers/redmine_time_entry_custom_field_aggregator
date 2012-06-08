b52feaba8a4baa405424a709594bd04f181ffaf6
yono <yono.05@gmail.com>
2012-06-08 18:07:17 +0900

    for Redmine 2.0.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
-ActionController::Routing::Routes.draw do |map|
-  map.with_options :controller => 'aggregator', :action => 'index' do |aggregator| 
-    aggregator.connect 'projects/:project_id/aggregator' 
-    aggregator.connect 'projects/:project_id/aggregator.:format'
+if Rails.version.to_f >= 3.0
+  scope :controller => 'aggregator', :action => 'index' do
+    match 'projects/:project_id/aggregator'
+    match 'projects/:project_id/aggregator.:format'
+  end
+else
+  ActionController::Routing::Routes.draw do |map|
+    map.with_options :controller => 'aggregator', :action => 'index' do |aggregator|
+      aggregator.connect 'projects/:project_id/aggregator'
+      aggregator.connect 'projects/:project_id/aggregator.:format'
+    end
   end
 end
